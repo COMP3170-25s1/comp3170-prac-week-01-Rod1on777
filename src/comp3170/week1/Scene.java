e7d2c483bc02ebc7239cdeffd1cfa833b7813021
Russ1an_spy <rodionk533@gmail.com>
2025-03-06 20:37:03 +1100

    prac1 complete

--- src/comp3170/week1/Scene.java
+++ src/comp3170/week1/Scene.java
@@ -39,13 +39,13 @@ public class Scene {
 		// @formatter:off
 		
 		vertices = new Vector4f[] {
-			new Vector4f(1.0f, 1.0f, 0.0f, 1.0f),
-			new Vector4f(-1.0f, 1.0f, 0.0f, 1.0f),
-			new Vector4f(-1.0f, -1.0f, 0.0f, 1.0f),
+			new Vector4f(-0.5f, 0.0f, 0.0f, 1.0f),
+			new Vector4f(0.0f, 0.5f, 0.0f, 1.0f),
+			new Vector4f(0.5f, 0.0f, 0.0f, 1.0f),
 			
-			new Vector4f(-1.0f, -1.0f, 0.0f, 1.0f),
-			new Vector4f(1.0f, -1.0f, 0.0f, 1.0f),
-			new Vector4f(1.0f, 1.0f, 0.0f, 1.0f),
+			new Vector4f(-0.5f, 0.0f, 0.0f, 1.0f),
+			new Vector4f(0.0f, -0.5f, 0.0f, 1.0f),
+			new Vector4f(0.5f, 0.0f, 0.0f, 1.0f),
 		};
 			
 		// @formatter:on
@@ -66,7 +66,7 @@ public class Scene {
 		shader.setAttribute("a_position", vertexBuffer);
 		
 		// write the colour value into the u_colour uniform. For now, we will write colours as Vec3s (R,G,B).
-		Vector3f colour = new Vector3f(1.0f, 0.0f, 0.f); // RED
+		Vector3f colour = new Vector3f(0.0f, 0.0f, 0.0f); // RED
 		shader.setUniform("u_colour", colour);
 		
 		Vector2f screenSize = new Vector2f(screenWidth, screenHeight);
--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -1,18 +1,23 @@
-#version 410
+#version 410  //GLSL version
 
 uniform vec3 u_colour;			// colour as a 3D vector (r,g,b)
 uniform vec2 u_screenSize;  	// screen dimensions in pixels
 
 layout(location = 0) out vec4 o_colour;	// output to colour buffer
 
-void main() {
+void main() {	//Main shader variables
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)
-   float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+  // float d = distance(p, vec2(0.5, 0.5));     // calculate distance to midpoint
+   vec2 v = abs(p - vec2(0.5, 0.5));
+   float d = max(v.x, v.y);
+   u_colour;
   
-   if (d < 0.5) {
-      o_colour = vec4(u_colour, 1);
+   vec3 finalColor = vec3(1, 0, 1);
+  
+   if (d < 0.4) {
+      o_colour = vec4(finalColor, 1);
    }
    else {
-      o_colour = vec4(0,0,0,1); // BLACK
+      o_colour = vec4(0.5,1,1,1); // BLACK
    }
 }
